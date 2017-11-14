dbd991b32936936a4124031573db975895c9f780
Henry Alpert <halpert3@gmail.com>
2017-11-14 11:02:35 -0600

    Add owner to database and forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
-  resources :dogs, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  # resources :dogs, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :dogs #this is the same as above
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
